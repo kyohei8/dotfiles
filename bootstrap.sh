34e60224e44d6cf13702c7ae7c0594a1f6c46c27
kyohei8 <tsukuda.kyouhei@gmail.com>
2016-05-02 15:34:50 +0900

    remove global_ignore setting

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,9 +25,8 @@ for dotfile in `ls -aF | grep '^\.'`; do
 done
 
 # add "global gitignore" alias
-ln -s `pwd`/'_gitignore' $HOME/'.global_ignore'
+ln -s `pwd`/'_gitignore' $HOME/'.gitignore'
 echo 'create symbolic link -> .global_ignore'
-git config --global core.excludesfile $HOME/.global_ignore
 #install brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #brewインストール
